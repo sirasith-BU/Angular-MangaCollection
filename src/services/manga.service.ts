b40d5d2d5d750219d87f12703b505ae3efeda528
Sirasith Klinfuengfu <sirasith.klin@bumail.net>
2025-04-23 11:53:08 +0700

    add p-toast

---
+++ src/services/manga.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CreateMangaDTO, UpdateMangaDTO } from '../interfaces/IManga';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class MangaService {
+  private mangaApiUrl = environment.apiUrl + '/api/manga';
+
+  constructor(private http: HttpClient) {}
+
+  getManga(): Observable<any> {
+    return this.http.get(this.mangaApiUrl);
+  }
+
+  getMangaAsync(id: number): Observable<any> {
+    return this.http.get(this.mangaApiUrl + `/${id}`);
+  }
+
+  createManga(data: CreateMangaDTO): Observable<any> {
+    return this.http.post(this.mangaApiUrl, data);
+  }
+
+  updateManga(data: UpdateMangaDTO, id: number): Observable<any> {
+    return this.http.put(this.mangaApiUrl + `/${id}`, data);
+  }
+
+  deleteManga(id: number): Observable<any> {
+    return this.http.delete(this.mangaApiUrl + `/${id}`);
+  }
+}
